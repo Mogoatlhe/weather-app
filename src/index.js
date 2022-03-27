327c55cad675e6624f995b204183e639fed85ed5
mogoatlhe <iam@ofent.se>
2022-03-27 14:17:28 +0200

    added the loader when waiting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Div from "./html/div";
 import Weather from "./weather/weather";
 import Attribute from "./html/attribute";
+import "./style/style.css";
 
 const content = document.getElementById("content");
 
@@ -9,7 +10,16 @@ const locationDivAttrArr = [locationDivIdAttr];
 const locationDiv = new Div(undefined, locationDivAttrArr);
 const locationDivNode = locationDiv.getDiv();
 
+const weatherDivIdAttr = new Attribute("id", "weather-div");
+const weatherDivIdAttrArr = [weatherDivIdAttr];
+const weatherDiv = new Div(undefined, weatherDivIdAttrArr);
+const weatherDivNode = weatherDiv.getDiv();
+
 const weather = new Weather();
 weather.setWeather();
+locationDivNode.addEventListener("click", () => {
+	weather.setWeather();
+});
 
 content.append(locationDivNode);
+content.append(weatherDivNode);
--- src/weather/weather.js
+++ src/weather/weather.js
@@ -1,12 +1,13 @@
 import "regenerator-runtime/runtime";
 
-/* eslint-disable lines-between-class-members */
 export default class Weather {
 	#weather;
+	#bodyNode;
 	#location;
 
 	constructor(location) {
 		this.#location = location;
+		this.#bodyNode = document.querySelector("body");
 	}
 
 	setLocation(location) {
@@ -19,6 +20,8 @@ export default class Weather {
 
 	async setWeather() {
 		try {
+			const childNodes = this.#removeChildNodes();
+			this.#bodyNode.classList.add("loader");
 			this.#weather = await fetch(
 				"https://api.openweathermap.org/data/2.5/weather?q=Pretoria&APPID=cdb642150b8871cb21adf69e7bd3ddd6",
 				{
@@ -27,8 +30,25 @@ export default class Weather {
 			);
 
 			this.#weather = await this.#weather.json();
+			this.#addChildren(childNodes);
+			this.#bodyNode.classList.remove("loader");
 		} catch (e) {
 			console.log(e);
 		}
+	}
+
+	#removeChildNodes() {
+		const children = this.#bodyNode.childNodes;
+		const childNodes = [];
+
+		[...children].forEach((child) =>
+			childNodes.push(this.#bodyNode.removeChild(child))
+		);
+
+		return childNodes;
+	}
+
+	#addChildren(children) {
+		[...children].forEach((child) => this.#bodyNode.append(child));
 	}
 }
